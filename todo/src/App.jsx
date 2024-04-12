80cd706f9e42f513d9b46d66383193bb6e8de8f9
DoE <kdhwh09@gmail.com>
2024-04-12 18:43:08 +0900

    CRUD todos (useReducer)

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useRef } from "react";
+import { useRef, useReducer } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -25,45 +25,48 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   // Create
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   // Update
   const onUpdate = (targetId) => {
-    // if문
-    // setTodos(
-    //   todos.map((todo) => {
-    //     if (todo.id === targetId) {
-    //       return { ...todo, isDone: !todo.isDone };
-    //     }
-    //     return todo;
-    //   })
-    // );
-
-    // 삼항 연산자
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
+    dispatch({ type: "UPDATE", targetId: targetId });
   };
 
   // Delete
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({ type: "DELETE", targetId: targetId });
   };
+
   return (
     <div className="App">
       <Header />
