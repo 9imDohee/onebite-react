059253edb59a254fdf6e903ceaf170ed20c62e96
DoE <kdhwh09@gmail.com>
2024-05-01 21:29:21 +0900

    Home 페이지 레이아웃 & 기능

--- project-diary/src/App.jsx
+++ project-diary/src/App.jsx
@@ -10,11 +10,24 @@ import NotFound from "./pages/NotFound";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdTime: new Date("2024-05-01").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
+  {
+    id: 2,
+    createdTime: new Date("2024-04-30").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdTime: new Date("2024-04-23").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
 ];
+
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE":
@@ -30,8 +43,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
